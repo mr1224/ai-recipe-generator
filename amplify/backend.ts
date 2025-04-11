01e8ddd6c9a9156b8a4df1df5639de12e2a9e91d
mr1224 <mrsim1224@gmail.com>
2025-04-11 14:00:00 +0900

    connect bedrock

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -5,7 +5,33 @@ import { data } from './data/resource';
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-defineBackend({
+import { defineBackend } from "@aws-amplify/backend";
+import { data } from "./data/resource";
+import { PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { auth } from "./auth/resource";
+
+const backend = defineBackend({
   auth,
   data,
 });
+
+const bedrockDataSource = backend.data.resources.graphqlApi.addHttpDataSource(
+  "bedrockDS",
+  "https://bedrock-runtime.ap-northeast-1.amazonaws.com",
+  {
+    authorizationConfig: {
+      signingRegion: "ap-northeast-1",
+      signingServiceName: "bedrock",
+    },
+  }
+);
+
+bedrockDataSource.grantPrincipal.addToPrincipalPolicy(
+  new PolicyStatement({
+    resources: [
+      "arn:aws:bedrock:ap-northeast-1::foundation-model/anthropic.claude-3-haiku-20240307-v1:0",
+    ],
+    actions: ["bedrock:InvokeModel"],
+    
+  })
+);
